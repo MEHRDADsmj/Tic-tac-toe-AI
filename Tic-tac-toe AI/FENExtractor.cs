4c7e5ca154d83f011831e0dbeb07a3d9fe742ca8
Mehrdad Jafari <asedmehrdad@gmail.com>
2023-04-03 11:13:29 +0330

    Add separate class for board

--- Tic-tac-toe AI/FENExtractor.cs
+++ Tic-tac-toe AI/FENExtractor.cs
@@ -3,24 +3,6 @@ using System.Collections.Generic;
 
 namespace Tic_tac_toe_AI
 {
-    
-    /// <summary>
-    /// T3 standing for tic-tac-toe
-    /// </summary>
-    public class T3Board
-    {
-        public List<char> board = new List<char>();
-        public bool isXToPlay = true;
-
-        public T3Board()
-        {
-            for (int index = 0; index < 9; ++index)
-            {
-                board.Add('+');
-            }
-        }
-    }
-    
     public static class FENExtractor
     {
         public static T3Board ExtractFEN(string FEN)
---
+++ Tic-tac-toe AI/T3Board.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Tic_tac_toe_AI
+{
+    /// <summary>
+    /// T3 standing for tic-tac-toe
+    /// </summary>
+    public class T3Board
+    {
+        public List<char> board = new List<char>();
+        public bool isXToPlay = true;
+
+        public T3Board()
+        {
+            for (int index = 0; index < 9; ++index)
+            {
+                board.Add('+');
+            }
+        }
+    }
+}
