3bd0f04ecf3ae7b1704ab7d93d23603828dd8d40
Mehrdad Jafari <asedmehrdad@gmail.com>
2023-04-03 11:11:47 +0330

    Add AI move picker

---
+++ Tic-tac-toe AI/AIMovePicker.cs
@@ -0,0 +1,25 @@
+﻿namespace Tic_tac_toe_AI
+{
+    public static class AIMovePicker
+    {
+        public static string FindBestMove(string currentFEN)
+        {
+            T3Board currentBoard = FENExtractor.ExtractFEN(currentFEN);
+            T3Board bestMove = null;
+            
+            // Do AI stuff
+            bestMove = EvaluateBoardMove(currentBoard);
+
+            return FENExtractor.ExportFEN(bestMove);
+        }
+
+        public static T3Board EvaluateBoardMove(T3Board currentBoard)
+        {
+            T3Board bestMove = null;
+
+
+
+            return bestMove;
+        }
+    }
+}
